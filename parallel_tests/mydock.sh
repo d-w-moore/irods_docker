228c7f7bc21a5e5bbd601dd6a264e404bcb4a1b9
d-w-moore <dmoore@renci.org>
2018-11-22 08:20:55 -0500

    specify suffix for image to run w/ rr iput

--- parallel_tests/mydock.sh
+++ parallel_tests/mydock.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
+suffix=""
 preargs=()
 while [ $# -ge 1 ]; do
   thing=$1 ; shift
   [ "$thing" = "--" ] && break
-  preargs+=("$thing")
+  case $thing in
+    --s=*) suffix=${thing#*=} ;;
+    *)  preargs+=("$thing");;
+  esac
 done
+
+#========================================================
+# make sure we are running the right docker image
+#========================================================
+echo >&2 "y to proceed , n to cancel, c to change suffix"
+echo >&2 "----------------------------------------------"
+ans=""
+while [ -z "$ans" ]; do
+  : "'${suffix:="4_2_s"}'"
+  read -p "Action (suffix='$suffix'): " ans
+  case $ans in
+    [nN]*) break;; [yY]*)  break;; 
+    [cC]*) read -p 'Enter new suffix -> ' suffix ; ans='';;
+  esac
+  ans=''
+done
+
+[[ $ans = [nN]* ]] && exit 0 
+#exit 123 # -- dwm
 
 #-- verify pre and postargs --
 #for x in "${preargs[@]}"; do
@@ -17,5 +40,5 @@ docker run -it \
   --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
   --security-opt seccomp=unconfined \
 "${preargs[@]}" \
-irods_test_env_4_2_s  $*
+irods_test_env_${suffix}  $*
 
