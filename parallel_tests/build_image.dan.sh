ccb5af4ff63049df8c15c2fa60a49e302c3b087d
d-w-moore <dmoore@renci.org>
2018-11-26 15:45:33 -0500

    build custom images by name

--- parallel_tests/build_image.dan.sh
+++ parallel_tests/build_image.dan.sh
@@ -1,10 +1,25 @@
 #! /bin/bash
 
+usage(){
+  echo >&2 "\
+  $0 <image_suffix>
+  ";exit 1
+}
+
+srcroot=$(dirname $0)/ttmp/
+status="Checking local irods local source directory"
+if [ ! -d $srcroot/irods ] ; then
+  status="!! Success !!"
+  echo >&2 -n "Trying to unpack irods source locally ... "
+  ( cd $srcroot && tar xf irodsSource.tgz 2>/dev/null ) || status="** FAILURE **"
+fi
+echo >&2 "$status"; [[ $status = *FAIL* ]] && exit 126
+#echo >&2 "continuing"; exit 0 # -- dwm
+
 if [ -n "$1" ]; then 
   DOCKVAR="$1"
   echo >&2 BUILDING IMAGE
   docker build --build-arg docker_var=$DOCKVAR -f Dockerfile.dan.$1 -t irods_test_env_$1 .
 else
-  echo >&2 "Please add an argument eg. 42s"
-  :
+  usage
 fi
