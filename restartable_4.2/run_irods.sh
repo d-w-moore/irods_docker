c9984aa1e4fc1f0f68998b452a20fe406dce0b2e
d-w-moore <dmoore@renci.org>
2019-02-02 14:20:52 -0500

    postgres & iRODS-4.2.x graceful shutdown & restart

---
+++ restartable_4.2/run_irods.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+service postgresql start
+
+until pg_isready -q 
+do
+  echo >&2 "Waiting for database ..."
+  sleep 1
+done
+
+pg_isready -q || { echo >&2 "Quit waiting for ICAT database"; exit 16; }
+
+if ! id -u irods >/dev/null 2>&1
+then
+
+  echo >&2 "Installing iRODS ..."
+  python /var/lib/irods/scripts/setup_irods.py < /var/lib/irods/packaging/localhost_setup_postgres.input
+
+else
+
+  echo >&2 "Starting up iRODS ..."
+  service irods start
+
+fi && echo >&2  $'\n --> IRODS Server is running \n'
+
+f() { trap -- INT; trap -- TERM; }
+trap 'f' TERM INT
+
+sleep $(( (1 << 31) - 1 )) # seconds to heat death in a 32-bit universe
+
+service irods stop && echo >&2 $'\n <-- IRODS Server is shut down' 
+service postgresql stop
