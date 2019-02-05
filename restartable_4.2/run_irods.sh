f2e56279573a73c968277c741156c9d3740c41fd
d-w-moore <dmoore@renci.org>
2019-02-05 08:13:45 -0500

    # This is a combination of 2 commits. # The first commit's message is:
    robust restart?
    
    # This is the 2nd commit message:
    
    robust restart #2 ?

--- restartable_4.2/run_irods.sh
+++ restartable_4.2/run_irods.sh
@@ -2,12 +2,14 @@
 
 service postgresql start
 
+# - parameters for waiting on DB to start
+timeout=30 
 tries=0
-until pg_isready -q 
+
+until pg_isready -q || [ $tries -ge $timeout ]
 do
   echo >&2 "Waiting for database ..." $((++tries))
   sleep 1
-  [ $tries -ge 16 ] && break
 done
 
 pg_isready -q || { echo >&2 "Quit waiting for ICAT database"; exit 16; }
@@ -25,10 +27,15 @@ else
 
 fi && echo >&2  $'\n --> IRODS Server is running \n'
 
-f() { trap -- INT; trap -- TERM; }
-trap 'f' TERM INT
+cleanup() 
+{
+    echo >&2 " ... attempting shutdown of irods and ICAT "
+    service irods stop && echo >&2 $'\n <-- IRODS Server is shut down' 
+    service postgresql stop
+}
+f() { trap -- EXIT; trap -- INT; trap -- TERM; cleanup; }
+
+trap 'f' TERM INT EXIT
 
 sleep $(( (1 << 31) - 1 ))'d' # a very long wait
 
-service irods stop && echo >&2 $'\n <-- IRODS Server is shut down' 
-service postgresql stop
